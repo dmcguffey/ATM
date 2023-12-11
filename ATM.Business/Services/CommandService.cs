e053d448206c7ec5ff8ebf86959fb93c98267578
David McGuffey <dmcguffey94@gmail.com>
2023-12-11 14:24:11 -0600

    added a deposit, withdraw and transferfunds method

---
+++ ATM.Business/Services/CommandService.cs
@@ -0,0 +1,23 @@
+﻿using ATM.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.models.models;
+
+namespace ATM.Business.Services
+{
+    internal class CommandService : ICommandService
+    {
+        public void deposit(decimal balance,decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void withdraw(decimal balance, decimal amount)
+        {
+            balance -= amount;
+        }
+    }
+}
