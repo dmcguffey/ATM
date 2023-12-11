3155665d3d5b2407cd0ad613a628ffb68db98207
David McGuffey <dmcguffey94@gmail.com>
2023-12-11 14:35:57 -0600

    created a new class CardCheck for validating cards

---
+++ ATM.Business/Services/CardCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Business.Services
+{
+    public class CardCheck
+    {
+        public CardCheck() { }
+        
+        public bool IsCard(int CardNumber)
+        {
+            //Apply Luhn's Algorithm for validating a real card
+            throw new NotImplementedException();
+        }
+    }
+}
