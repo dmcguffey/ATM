f26f4ec0bb29df4c677b67669048d5d58f0757cd
David McGuffey <dmcguffey94@gmail.com>
2023-12-19 14:01:25 -0600

    adding comments for reading algorithm

--- ATM.Business/Services/CardCheck.cs
+++ ATM.Business/Services/CardCheck.cs
@@ -10,22 +10,41 @@ namespace ATM.Business.Services
     {
         public CardCheck() { }
         
-        public static bool IsCard(string CardNumber)
+        public static bool IsCard(string cardNumber)
         {
+            //sum will be what is used to determine if number is divisible by 10
             int sum = 0;
+            //for determining if multiplying the digit by two is double digit
+            bool doubleDigit = false;
+            //create a new array to go through. Reversing the array allows for easier processing.
+            char[] cardDigits = cardNumber.ToCharArray();
+            Array.Reverse(cardDigits);
 
-            foreach(var digit in CardNumber.Split(','))
+            foreach(var digit in cardDigits)
             {
-                var number = int.Parse(digit);
-                if (number % 2 != 0)
+                //must check if number is a digit
+                if (char.IsDigit(digit))
                 {
-                    number *= 2;
+                    //convert to int
+                    int number = digit - '0';
+                    //check if number is a double digit
+                    if(doubleDigit)
+                    {
+                        number *= 2;
+                        if (number > 9)
+                        {
+                            number -= 9;
+                        }
+                    }
+                    sum += number;
+                    doubleDigit = !doubleDigit;
                 }
-                sum = number / 10;
-                sum = number % 10;
+                else if (digit != ' ')
+                {
+                    throw new ArgumentException("Invalid Entry.");
+                }
 
             }
-
             return (sum % 10 == 0);
         }
     }
