2524994d213119d88321a9be3dd185efb2defaa5
David McGuffey <dmcguffey94@gmail.com>
2024-01-28 15:05:19 -0600

    added a false and true test for checking the card number

--- ATM.Tests/Tests/CardCheckTest.cs
+++ ATM.Tests/Tests/CardCheckTest.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using ATM.Business.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ATM.Tests.Tests
 {
-    internal class CardCheckTest
+    public class CardCheckTest
     {
+        [Theory]
+        [InlineData("4111111111111111", true)]
+
+        public void IsCard(string cardNumber, bool expected)
+        {
+            //ARRANGE not necessary with static
+            //ACT
+            bool actual = CardCheck.IsCard(cardNumber);
+            //ASSERT
+            Assert.Equal(expected, actual);
+        }
     }
 }
